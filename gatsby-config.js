1eaf452c3c9e93756d01bad56a71b4ec4db3bc7f
mtane0412 <mtane0412@mtane0412MacbookPro.local>
2020-11-23 16:52:04 +0900

    add gatsby-remark-embedder

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,9 @@ module.exports = {
               escapeEntities: {},
             },
           },
+          {
+            resolve: `gatsby-remark-embedder`,
+          },
         ],
       },
     },
