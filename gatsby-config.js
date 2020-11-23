7d14d67b30bb56f0aa831cca2f58d34d80a804a9
mtane0412 <mtane0412@mtane0412MacbookPro.local>
2020-11-23 14:16:51 +0900

    add gatsby-remark-prismjs

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,5 +38,42 @@ module.exports = {
       options: contentfulConfig,
     },
     "gatsby-plugin-twitter",
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+              languageExtensions: [
+                {
+                  language: "superscript",
+                  extend: "javascript",
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+              prompt: {
+                user: "root",
+                host: "localhost",
+                global: false,
+              },
+              escapeEntities: {},
+            },
+          },
+        ],
+      },
+    },
   ],
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './base.css'
 import Container from './container'
 import Navigation from './navigation'
+import "prismjs/themes/prism-tomorrow.css"
 
 class Template extends React.Component {
   render() {
