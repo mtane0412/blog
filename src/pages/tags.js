18f03779eff0543b866922bf50ea414abbb2d0c9
mtane0412 <mtane0412@gmail.com>
2021-01-11 11:27:17 +0900

    不要なMutationの削除

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -43,9 +43,6 @@ export const pageQuery = graphql`
         totalCount
       }
     }
-    contentfulTags(slug: { eq: "bike" }) {
-      title
-    }
     allContentfulTags {
       edges {
         node {
